3554176a427491d5f07571a065143b8978c41860
Sola Micheal <solamicheal2002@gmail.com>
2017-03-21 16:36:30 +0100

    new work

---
+++ For iteration/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++) 
+            {
+                if (i == 7) 
+                {
+                    Console.WriteLine("David is HOME\a");
+                    break;
+                }
+            }
+
+
+
+
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+            } 
+               
+
+            Console.ReadLine();
+        }
+    }
+}
